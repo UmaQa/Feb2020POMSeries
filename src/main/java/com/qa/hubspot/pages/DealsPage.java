ede5346c9c12b596f8710398e7770a01627a7006
UmaQa <umaqa17@gmail.com>
2020-05-02 01:46:41 -0400

    added dealspage class and upded signup link locator link locator in login page

---
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -0,0 +1,13 @@
+package com.qa.hubspot.pages;
+
+import com.qa.hubspot.base.BasePage;
+
+public class DealsPage extends BasePage{
+	
+	public DealsPage() {
+		System.out.println("DealsPAge constructor..");
+		
+	}
+	
+
+}
